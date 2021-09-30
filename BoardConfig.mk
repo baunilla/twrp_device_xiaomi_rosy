9df8450ea6d1a49a6035ad2359ed8e10e4694567
Aseem Athale <athaleaseem@gmail.com>
2021-09-30 14:43:45 +0530

    rosy: Remove unnecessary modules
    Ashmemd isn't needed in 11 tree. Also, removed duplicated modules in
    device.mk.
    
    Change-Id: Iab52ff5b51d1673decf3b4000505e210f4b8e8df

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -119,30 +119,16 @@ TW_HAS_EDL_MODE := false
 # Disable Mouse Cursor
 TW_INPUT_BLACKLIST := "hbtp_vm"
 
+# Additional binaries & libraries needed for recovery
 TARGET_RECOVERY_DEVICE_MODULES += \
-    ashmemd \
-    ashmemd_aidl_interface-cpp \
-    libashmemd_client \
+    libandroidicu \
     libcap \
     libdrm \
-    libicui18n \
     libion \
-    libicuuc \
-    libpcrecpp \
-    libprocinfo \
     libxml2
 
-TW_RECOVERY_ADDITIONAL_RELINK_BINARY_FILES += \
-    $(TARGET_OUT_EXECUTABLES)/ashmemd
-
 TW_RECOVERY_ADDITIONAL_RELINK_LIBRARY_FILES += \
-    $(TARGET_OUT_SHARED_LIBRARIES)/ashmemd_aidl_interface-cpp.so \
-    $(TARGET_OUT_SHARED_LIBRARIES)/libashmemd_client.so \
     $(TARGET_OUT_SHARED_LIBRARIES)/libcap.so \
     $(TARGET_OUT_SHARED_LIBRARIES)/libdrm.so \
-    $(TARGET_OUT_SHARED_LIBRARIES)/libicui18n.so \
     $(TARGET_OUT_SHARED_LIBRARIES)/libion.so \
-    $(TARGET_OUT_SHARED_LIBRARIES)/libicuuc.so \
-    $(TARGET_OUT_SHARED_LIBRARIES)/libpcrecpp.so \
-    $(TARGET_OUT_SHARED_LIBRARIES)/libprocinfo.so \
     $(TARGET_OUT_SHARED_LIBRARIES)/libxml2.so
--- device.mk
+++ device.mk
@@ -31,5 +31,6 @@ PRODUCT_PACKAGES += \
     qcom_decrypt
 
 # Apex libraries
-PRODUCT_HOST_PACKAGES += \
-    libandroidicu
+PRODUCT_COPY_FILES += \
+    $(OUT_DIR)/target/product/$(PRODUCT_RELEASE_NAME)/obj/SHARED_LIBRARIES/libandroidicu_intermediates/libandroidicu.so:$(TARGET_COPY_OUT_RECOVERY)/root/system/lib64/libandroidicu.so
+
