82307978fbc17540b5f16ec4e7f3e064b4ee0ce0
Captain Throwback <captainthrowback@hotmail.com>
2022-02-21 10:52:38 -0500

    rosy: Use fscrypt policy v1
    Change-Id: I96bc751e1acae94619249081d627513007cd9e27

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -115,6 +115,7 @@ TW_NEW_ION_HEAP := true
 TW_EXCLUDE_TWRPAPP := true
 TW_INCLUDE_REPACKTOOLS := true
 TW_HAS_EDL_MODE := false
+TW_USE_FSCRYPT_POLICY := 1
 
 # Disable Mouse Cursor
 TW_INPUT_BLACKLIST := "hbtp_vm"
